c59a421ddb2e9d06ba1e47fe9334c10a508fab21
juanlod <juancorvelle@gmail.com>
2020-03-27 12:14:47 +0100

    actualizacion properties

--- springboot-servicio-item/src/main/java/com/jaunos/app/item/controller/ItemController.java
+++ springboot-servicio-item/src/main/java/com/jaunos/app/item/controller/ItemController.java
@@ -1,9 +1,16 @@
 package com.jaunos.app.item.controller;
 
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
+import org.springframework.core.env.Environment;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
@@ -12,35 +19,57 @@ import com.jaunos.app.item.model.Item;
 import com.jaunos.app.item.model.Producto;
 import com.jaunos.app.item.services.ItemService;
 import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
+import com.netflix.ribbon.proxy.annotation.Http;
 
+@RefreshScope
 @RestController
 public class ItemController {
-	
+
 	@Autowired
 	@Qualifier("serviceFeign")
 	private ItemService itemService;
 	
+	@Autowired
+	private Environment env;
+
+	@Value("${configuracion.texto}")
+	private String texto;
+
 	@GetMapping("/listar")
 	public List<Item> listar() {
 		return itemService.findAll();
 	}
-	
-	@HystrixCommand(fallbackMethod="metodoAlternativo")
+
+	@HystrixCommand(fallbackMethod = "metodoAlternativo")
 	@GetMapping("/ver/{id}/cantidad/{cantidad}")
 	public Item detalle(@PathVariable Long id, @PathVariable Integer cantidad) {
 		return itemService.findById(id, cantidad);
 	}
-	
+
 	// Se genera un método por defecto para tolerancia a fallos con Hystrix
 	public Item metodoAlternativo(Long id, Integer cantidad) {
 		Item item = new Item();
 		Producto producto = new Producto();
-		
+
 		item.setCantidad(cantidad);
 		producto.setId(id);
 		producto.setNombre("Camara Sony");
 		producto.setPrecio(500.00);
 		item.setProducto(producto);
-		return item; 
+		return item;
+	}
+
+	@GetMapping("/obtener-config")
+	public ResponseEntity<?> obtenerConfiguracion(@Value("${server.port}") String puerto) {
+		Map<String, String> json = new HashMap<String, String>();
+		json.put("texto", texto);
+		json.put("puerto", puerto);
+		
+		// Se comprueba que no sea nulo y que el entorno sea el de desarrollo
+		if (env.getActiveProfiles().length > 0 && env.getActiveProfiles()[0].equals("dev")) {
+			json.put("autor.nombre", env.getProperty("configuracion.autor.nombre"));
+			json.put("autor.email", env.getProperty("configuracion.autor.email"));
+		}
+		return new ResponseEntity<Map<String, String>>(json, HttpStatus.OK);
 	}
 }
